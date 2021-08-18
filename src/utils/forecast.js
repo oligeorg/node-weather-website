3246b2b554516bebeeaf0a3f6e315917b948b8d5
Ólafur Georg <oligeorg@hotmail.com>
2021-08-18 10:22:41 +0000

    Add more information to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find loaction', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The wind speed is ' + body.current.wind_speed + ' km/h. The air humidity level is ' + body.current.humidity + '%. The precipitation level is ' + body.current.precip + ' mm.')
         }
     })
 }
